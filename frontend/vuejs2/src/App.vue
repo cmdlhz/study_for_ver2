8843dcd071f0a4939dd5570d0fc995753f87d0d7
cmdlhz <cmdlhz@gmail.com>
2019-04-12 23:57:25 +0900

    [1-1] Nesting Components
    [Net Ninja] VueJS2 Tutorial #21  : Nesting Components
    
    https://youtu.be/6-us2D7GQCk

---
+++ frontend/vuejs2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Ninjas from './components/Ninjas.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components:{
+    'app-header': Header,
+    'app-ninjas': Ninjas,
+    'app-footer': Footer
+  },
+  data(){
+    return{
+      title: 'Ninja app'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: purple;
+}
+</style>
