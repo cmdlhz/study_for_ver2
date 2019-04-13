006c16f1c035a771e7755f5f668889488585dfc8
cmdlhz <cmdlhz@gmail.com>
2019-04-13 18:08:24 +0900

    [1-1] BLOG - Registering Things Locally
    [Net Ninja] VueJS2 Tutorial #37 :
    BLOG - Registering Things Locally
    
    https://youtu.be/T5h6L5MFLRA

--- frontend/vuejs2/src/components/ShowBlogs.vue
+++ frontend/vuejs2/src/components/ShowBlogs.vue
@@ -3,7 +3,7 @@
     <h1>All Blog Articles</h1>
     <input type="text" v-model="search" placeholder="SEARCH Blogs :)">
     <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
-      <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+      <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
@@ -34,6 +34,17 @@ export default {
       return this.blogs.filter(blog => {
         return blog.title.match(this.search) || blog.body.match(this.search);
       });
+    }
+  },
+  filters: {
+    toUppercase: value => value.toUpperCase(),
+    snippet: value => value.slice(0,100) + '...'
+  },
+  directives: {
+    rainbow: {
+      bind(el){
+        el.style.color = '#' + Math.random().toString().slice(2,8);
+      }
     }
   }
 }
--- frontend/vuejs2/src/main.js
+++ frontend/vuejs2/src/main.js
@@ -8,11 +8,6 @@ Vue.prototype.$http = axios;
 export const bus = new Vue();
 
 // Custom Directives
-Vue.directive('rainbow', {
-  bind(el){
-    el.style.color = '#' + Math.random().toString().slice(2,8);
-  }
-});
 Vue.directive('theme', {
   bind(el,binding){
     if(binding.value=='wide'){
@@ -28,8 +23,8 @@ Vue.directive('theme', {
 });
 
 // Filters
-Vue.filter('to-uppercase', value => value.toUpperCase());
-Vue.filter('snippet', value => value.slice(0,100) + '...');
+// Vue.filter('to-uppercase', value => value.toUpperCase());
+// Vue.filter('snippet', value => value.slice(0,100) + '...');
 
 new Vue({
   render: h => h(App),
