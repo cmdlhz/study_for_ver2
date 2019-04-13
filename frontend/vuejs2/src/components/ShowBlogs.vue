ecd60da301451b7409fa11ab87e6cc6d7574b777
cmdlhz <cmdlhz@gmail.com>
2019-04-13 17:59:21 +0900

    [1-1] BLOG - Custom Search Filter
    [Net Ninja] VueJS2 Tutorial #36 :
    BLOG - Custom Search Filter
    
    https://youtu.be/G34_yNV8FMY

--- frontend/vuejs2/src/components/ShowBlogs.vue
+++ frontend/vuejs2/src/components/ShowBlogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div v-for="blog in blogs" :key="blog.id" class="single-blog">
+    <input type="text" v-model="search" placeholder="SEARCH Blogs :)">
+    <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
@@ -13,7 +14,8 @@
 export default {
   data(){
     return{
-      blogs: []
+      blogs: [],
+      search: ''
     }
   },
   methods:{
@@ -24,8 +26,15 @@ export default {
     axios.get('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
       console.log(response);
-      this.blogs = response.data.slice(0,3);
-    })
+      this.blogs = response.data.slice(0,10);
+    });
+  },
+  computed:{
+    filteredBlogs: function(){
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search) || blog.body.match(this.search);
+      });
+    }
   }
 }
 </script>
