c69a7fdaa7a46ddabacdcb5d41b737b4ace10479
cmdlhz <cmdlhz@gmail.com>
2019-04-13 21:28:32 +0900

    [1-1] BLOG - Hash vs History (Routing) | BLOG - Adding Router Links
    [Net Ninja] VueJS2 Tutorial #40 : BLOG - Hash vs History (Routing)
    
    https://youtu.be/js5Sr_ZSHDs
    ---
    [Net Ninja] VueJS2 Tutorial #41 : BLOG - Adding Router Links
    
    https://youtu.be/yn0_6T4HwHs

--- frontend/vuejs2/src/App.vue
+++ frontend/vuejs2/src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import addBlog from './components/AddBlog.vue';	
+import showBlogs from './components/ShowBlogs.vue';
+import listBlogs from './components/ListBlogs.vue';
+import header from './components/Header.vue';
+
+export default {	
+  components:{
+    'add-blogs': addBlog,
+    'show-blogs': showBlogs,	    
+    'list-blogs': listBlogs,
+    'app-header': header
+  }	  
+}	
+</script>	
+
 
 <style scoped>
 body{
--- frontend/vuejs2/src/components/Header.vue
+++ frontend/vuejs2/src/components/Header.vue
@@ -1,40 +1,41 @@
 <template>
-  <header>
-		<h1 @click="changeTitle">{{ title }}</h1>
-	</header>
+  <nav>
+		<ul>
+			<li><router-link to="/" exact>Blog</router-link></li>
+			<li><router-link to="/add" exact>Add a Blog</router-link></li>
+		</ul>
+	</nav>
 </template>
 
 <script>
-import { bus } from '../main';
-
 export default {
-	props: {
-		title: {
-			type: String
-		}
-	},
-	data(){
-		return{
-			title: 'Vue Ninjas'
-		}
-	},
-	methods: {
-		changeTitle: function(){
-			// this.$emit('changeTitle', 'Vue Wizaarrrrds!!!')
-			this.title = 'Vue Wizaarrrrds!!!';
-			bus.$emit('titleChanged', 'Vue Wizaarrrrds!!!');
-		}
-	}
+
 }
 </script>
 
 <style scoped>
-header{
-	background: lightgreen;
-	padding: 10px;
-}
-h1{
-	color: #222;
+ul{
+	list-style-type: none;
 	text-align: center;
+	margin: 0;
+}
+li{
+	display: inline-block;
+	margin: 0 10px;
+}
+a{
+	color: #fff;
+	text-decoration: none;
+	padding: 6px 8px;
+	border-radius: 10px;
+}
+nav{
+	background: #444;
+	padding: 14px 0;
+	margin-bottom: 40px;
+}
+.router-link-active{
+	background: #eee;
+	color: #444;
 }
 </style>
