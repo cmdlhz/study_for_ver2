1e2fb9cb49023bc55a154c2e46ecd316da276667
cmdlhz <cmdlhz@gmail.com>
2019-04-13 16:45:38 +0900

    Revert "[1-1] Custom Directives"
    This reverts commit 33b30d056c957dbc9617c26a4bc796d806137904.

--- frontend/vuejs2/src/main.js
+++ frontend/vuejs2/src/main.js
@@ -7,26 +7,6 @@ Vue.prototype.$http = axios;
 
 export const bus = new Vue();
 
-// Custom Directives
-Vue.directive('rainbow', {
-  bind(el){
-    el.style.color = '#' + Math.random().toString().slice(2,8);
-  }
-});
-Vue.directive('theme', {
-  bind(el,binding){
-    if(binding.value=='wide'){
-      el.style.maxWidth = '1200px';
-    } else if(binding.value=='narrow'){
-      el.style.maxWidth = '600px';
-    }
-    if(binding.arg =='column'){
-      el.style.background = '#ddd';
-      el.style.padding = '20px';
-    }
-  }
-});
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
