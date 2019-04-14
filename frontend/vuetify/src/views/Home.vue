1e91e63ccf86016c9d96704b0afdb55a3dad2dec
cmdlhz <cmdlhz@gmail.com>
2019-04-14 23:19:13 +0900

    [1-3] Breakpoints & Visibility
    [Net Ninja] Vuex Tutorial 6 :  Breakpoints & Visibility
    
    https://youtu.be/55hSO_LJeCo

--- frontend/vuetify/src/views/Home.vue
+++ frontend/vuetify/src/views/Home.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="home">
-    <h1>Homepage</h1>
-    <v-btn class="blue lighten-1 white--text">CLICK ME</v-btn>
-    <v-btn depressed color="blue lighten-1">CLICK ME</v-btn>
-    <v-btn flat color="blue lighten-1">CLICK ME</v-btn>
-
-    <v-btn depressed class="blue lighten-1 white--text">
-      <v-icon left>email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-    <v-btn depressed small class="blue lighten-1 white--text">
-      <v-icon left small>email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-    <v-btn large class="blue lighten-1 white--text">
-      <span>EMAIL ME</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-    <v-btn fab depressed small dark color="red darken-1">
-      <v-icon>favorite</v-icon>
-    </v-btn>
-    <v-btn fab small dark color="red darken-1">
-      <v-icon>favorite</v-icon>
-    </v-btn>
+    <v-btn class="hidden-xs-only">SMALL AND UP</v-btn>
+    <v-btn class="hidden-sm-and-up">XS ONLY</v-btn>
   </div>
 </template>
 
