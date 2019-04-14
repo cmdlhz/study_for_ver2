d6dedd03673cc1d37e94c2c58e19bf2cda9a38e0
cmdlhz <cmdlhz@gmail.com>
2019-04-14 20:59:43 +0900

    [1-3] Text & Colours
    [Net Ninja] Vuex Tutorial 4 : Text & Colours
    
    https://youtu.be/BIuQfQcptIU

--- frontend/vuetify/src/views/Home.vue
+++ frontend/vuetify/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="blue darken-4 white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet iusto tempora esse neque tempore! Dolorum officiis, est explicabo omnis nihil esse vitae voluptas excepturi suscipit. Sint officiis quis laudantium.</p>
+    <p class="light-green lighten-3 green--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet iusto tempora esse neque tempore! Dolorum officiis, est explicabo omnis nihil esse vitae voluptas excepturi suscipit. Sint officiis quis laudantium.</p>
+
+    <h1 class="display-4">Massive TEXT</h1>
+    <h4 class="display-1">Smaller TEXT</h4>
+
+    <p class="headline">This is a headline.</p>
+    <p class="subheading font-weight-bold">This is a subheading.</p>
+    <p class="caption">This is a caption.</p>
   </div>
 </template>
 
