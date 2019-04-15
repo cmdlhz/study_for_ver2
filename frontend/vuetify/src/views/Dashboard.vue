201c2e6ab44f2dedc7dc4a8c69e84781045092f7
cmdlhz <cmdlhz@gmail.com>
2019-04-15 15:42:24 +0900

    [1-3] Grid System (Part 1)
    [Net Ninja] Vuex Tutorial 13 : Grid System (Part 1)
    
    https://youtu.be/0Qv-6fQBFfA

--- frontend/vuetify/src/views/Dashboard.vue
+++ frontend/vuetify/src/views/Dashboard.vue
@@ -3,8 +3,29 @@
     <h1 class="subheading grey--text">Dashboard</h1>
     <!-- Add "fluid" full width / smaller margins -->
     <v-container class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laudantium, dolores debitis ad in voluptate temporibus ratione qui incidunt sequi nemo et doloribus ut porro dolore blanditiis veritatis quidem ducimus.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laudantium, dolores debitis ad in voluptate temporibus ratione qui incidunt sequi nemo et doloribus ut porro dolore blanditiis veritatis quidem ducimus.</p>
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">ONE</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">TWO</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">THREE</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">FOUR</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">FIVE</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">SIX</v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
