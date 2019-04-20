c113e0088c9d89e08b157963d163bb85bae2f843
cmdlhz <cmdlhz@gmail.com>
2019-04-20 16:47:42 +0900

    updated comments

--- codingChallenges/CodeWars/8 kyu/1_sumArrays.js
+++ codingChallenges/CodeWars/8 kyu/1_sumArrays.js
@@ -1,6 +1,7 @@
 /*
 Sum Array
-  Write a method sum (sum_array in python, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
+  Write a method sum (sum_array in python, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. 
+  These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
 
 Examples
   numbers = [1, 5.2, 4, 0, -1]
