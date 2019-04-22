7934641276cf3cdd43ac43b48cea845d02272720
cmdlhz <cmdlhz@gmail.com>
2019-04-22 23:03:42 +0900

    [2-1] Maps
    Python 3 Tutorial for Beginners #22 - Maps
    
    https://youtu.be/3Da00hgQ834

---
+++ backend/python/python3/practice/maps.py
@@ -0,0 +1,23 @@
+from random import shuffle
+
+def jumble(word):
+  # 'blue' => 'b','l','u','e'
+  anagram = list(word) 
+  # Mix the order => 'l','u','b','e'
+  shuffle(anagram)
+  # join the letters => 'lube'
+  return ''.join(anagram)
+
+words = ['yellow', 'blue', 'green']
+anagrams = []
+
+## for loop
+for word in words:
+  anagrams.append(jumble(word))
+print(anagrams)
+
+## Comprehension way
+print( [jumble(word) for word in words] )
+
+## map(function, data)
+print(list(map(jumble, words)))
