aaa142684d5e21e9fac4ffafee14ce91a932d491
cmdlhz <cmdlhz@gmail.com>
2019-04-22 21:41:05 +0900

    [2-1] Methods & Attributes
    Python 3 Tutorial for Beginners #18 - Methods & Attributes
    
    https://youtu.be/LwFnF9XoEfM

--- backend/python/python3/practice/classes.py
+++ backend/python/python3/practice/classes.py
@@ -1,5 +1,17 @@
 class Planet:
 
+  # Class level attributes
+  shape = 'round'
+
+  @classmethod
+  def commons(cls):
+    return f'All planets are {cls.shape} because of gravity.'
+
+  @staticmethod
+  def spin(speed = '2000 miles per hour'):
+    return f'The planet spins and spins at {speed}.'
+
+  # Instance level attributes
   def __init__(self, name, radius, gravity, system):
     self.name = name
     self.radius = radius
@@ -20,3 +32,6 @@ print(f'Name is: {naboo.name}.')
 print(f'Radius is: {naboo.radius}.')
 print(f'Gravity is: {naboo.gravity}.')
 print(naboo.orbit())
+
+print(naboo.commons())
+print(Planet.spin('a very high speed'))
