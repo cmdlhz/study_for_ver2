aae2fa342322924a25aac83b09b0c606b8bb97fd
cmdlhz <cmdlhz@gmail.com>
2019-04-22 22:03:03 +0900

    [2-1] Modules & Packages
    Python 3 Tutorial for Beginners #19 - Modules & Packages
    
    https://youtu.be/f26nAmfJggw

--- backend/python/python3/practice/classes.py
+++ backend/python/python3/practice/classes.py
@@ -1,37 +1,10 @@
-class Planet:
-
-  # Class level attributes
-  shape = 'round'
-
-  @classmethod
-  def commons(cls):
-    return f'All planets are {cls.shape} because of gravity.'
-
-  @staticmethod
-  def spin(speed = '2000 miles per hour'):
-    return f'The planet spins and spins at {speed}.'
-
-  # Instance level attributes
-  def __init__(self, name, radius, gravity, system):
-    self.name = name
-    self.radius = radius
-    self.gravity = gravity
-    self.system = system
-
-  def orbit(self):
-    return f'{self.name} is orbiting in the {self.system}.'
+from space.planet import Planet
+from space.calc import planet_mass, planet_vol
 
 hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
-print(f'Name is: {hoth.name}.')
-print(f'Radius is: {hoth.radius}.')
-print(f'Gravity is: {hoth.gravity}.')
-print(hoth.orbit())
-
 naboo = Planet('Naboo', 300000, 8, 'Naboo System')
-print(f'Name is: {naboo.name}.')
-print(f'Radius is: {naboo.radius}.')
-print(f'Gravity is: {naboo.gravity}.')
-print(naboo.orbit())
 
-print(naboo.commons())
-print(Planet.spin('a very high speed'))
+naboo_mass = planet_mass(naboo.gravity, naboo.radius)
+naboo_vol = planet_vol(naboo.radius)
+
+print(f'{naboo.name} has a mass of {naboo_mass} and a volumn of {naboo_vol}.')
