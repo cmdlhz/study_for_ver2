400dd815cfdf2a50684eacf224a2463786644ced
cmdlhz <cmdlhz@gmail.com>
2019-04-22 19:56:25 +0900

    [2-1] For Loops
    Python 3 Tutorial for Beginners #9 - For Loops
    
    https://youtu.be/ENMCxQ9kNS4

---
+++ backend/python/python3/practice/loops.py
@@ -0,0 +1,11 @@
+colors = ['red', 'yellow', 'green', 'blue']
+
+# for color in colors[1:3]:
+#   print(color)
+
+for color in colors:
+  if color == 'green': 
+    print(f'Her dress is {color}.')
+    break
+  else:
+    print(color)
