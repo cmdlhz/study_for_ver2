79125a7f731f1b05d1795364fc5903db10ad9729
cmdlhz <cmdlhz@gmail.com>
2019-04-23 17:39:53 +0900

    [2-2] Django Apps
    Django Tutorial #5 - Django Apps
    
    https://youtu.be/W5Gjcs6QwEs

--- backend/django_blog/django_blog/urls.py
+++ backend/django_blog/django_blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   re_path(r'^admin/', admin.site.urls),
   re_path(r'^$', views.homepage),
-  re_path(r'^about/$', views.about)
+  re_path(r'^about/$', views.about),
+  re_path(r'^articles/', include('articles.urls'))
 ]
