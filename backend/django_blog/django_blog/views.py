e2dc423deae6503032d477bfae8d959ebc0265cb
cmdlhz <cmdlhz@gmail.com>
2019-04-23 15:56:36 +0900

    [2-2] HTML Templates
    Django Tutorial #4 - HTML Templates
    
    https://youtu.be/Iy4niMCsbEE

--- backend/django_blog/django_blog/views.py
+++ backend/django_blog/django_blog/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-  return HttpResponse('HOMEPAGE')
-  
+  return render(request, 'homepage.html')
+
 def about(request):
-  return HttpResponse('ABOUT')
+  return render(request, 'about.html')
