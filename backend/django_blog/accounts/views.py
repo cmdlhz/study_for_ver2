d022006bec7e46a8680b1f73423e8f8ae4210b8f
cmdlhz <cmdlhz@gmail.com>
2019-04-24 02:05:23 +0900

    [2-2] Saving Users
    Django Tutorial #20 - Saving Users
    
    https://youtu.be/Xj0MXHrAi9o

--- backend/django_blog/accounts/views.py
+++ backend/django_blog/accounts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-  userForm = UserCreationForm()
+  if request.method == 'POST':
+    userForm = UserCreationForm(request.POST)
+    if userForm.is_valid():
+      userForm.save()
+      # log the user in
+      return redirect('articles:list')
+  else:
+    userForm = UserCreationForm()
   return render(request, 'accounts/signup.html', { 'form': userForm })
