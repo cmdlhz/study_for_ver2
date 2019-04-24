6eb8c5a91dcbe990dc1338133c4116746dc28104
cmdlhz <cmdlhz@gmail.com>
2019-04-24 11:43:02 +0900

    [2-2] Login Form
    Django Tutorial #21 - Login Form
    
    https://youtu.be/drMr9B3ZcPI

--- backend/django_blog/accounts/views.py
+++ backend/django_blog/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
@@ -12,3 +12,13 @@ def signup_view(request):
   else:
     userForm = UserCreationForm()
   return render(request, 'accounts/signup.html', { 'form': userForm })
+
+def login_view(request):
+  if request.method == 'POST':
+    authForm = AuthenticationForm(data=request.POST)
+    if authForm.is_valid():
+      # log in the user
+      return redirect('articles:list')
+  else:
+    authForm = AuthenticationForm()
+  return render(request, 'accounts/login.html', { 'form': authForm })
