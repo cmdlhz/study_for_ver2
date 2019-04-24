3cf76d85a8998182b04d2d2627420b8221527eb5
cmdlhz <cmdlhz@gmail.com>
2019-04-24 11:58:16 +0900

    [2-2] Logging Users In
    Django Tutorial #22 - Logging Users In
    
    https://youtu.be/XMgF3JwKzgs

--- backend/django_blog/accounts/views.py
+++ backend/django_blog/accounts/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
   if request.method == 'POST':
-    userForm = UserCreationForm(request.POST)
-    if userForm.is_valid():
-      userForm.save()
-      # log the user in
+    signupForm = UserCreationForm(request.POST)
+    if signupForm.is_valid():
+      user = signupForm.save()
+      # log in the user
+      login(request, user)
       return redirect('articles:list')
   else:
-    userForm = UserCreationForm()
-  return render(request, 'accounts/signup.html', { 'form': userForm })
+    signupForm = UserCreationForm()
+  return render(request, 'accounts/signup.html', { 'form': signupForm })
 
 def login_view(request):
   if request.method == 'POST':
-    authForm = AuthenticationForm(data=request.POST)
-    if authForm.is_valid():
+    loginForm = AuthenticationForm(data=request.POST)
+    if loginForm.is_valid():
       # log in the user
+      user = loginForm.get_user()
+      login(request, user)
       return redirect('articles:list')
   else:
-    authForm = AuthenticationForm()
-  return render(request, 'accounts/login.html', { 'form': authForm })
+    loginForm = AuthenticationForm()
+  return render(request, 'accounts/login.html', { 'form': loginForm })
