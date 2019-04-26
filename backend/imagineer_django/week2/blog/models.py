b2e2210a1696d6cfe87615bded6ef81eefd9a634
cmdlhz <cmdlhz@gmail.com>
2019-04-26 23:52:34 +0900

    [2-3] Week 2 admin.py

--- backend/imagineer_django/week2/blog/models.py
+++ backend/imagineer_django/week2/blog/models.py
@@ -6,6 +6,13 @@ class Article(models.Model):
   contents = models.TextField()
   view_count = models.IntegerField()
 
+  def __str__(self):
+    return self.title
+
+  objects = models.Manager()
+
 class Comment(models.Model):
   article = models.ForeignKey(Article, on_delete=models.CASCADE)
   comment = models.CharField(max_length=100)
+
+  objects = models.Manager()          
--- backend/imagineer_django/week2/blog/views.py
+++ backend/imagineer_django/week2/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
-  name = "Jen"
-  return render(request, 'index.html', { 'name' : name })
+  article_list = Article.objects.all()
+  return render(request, 'index.html', { 'article_list' : article_list })
