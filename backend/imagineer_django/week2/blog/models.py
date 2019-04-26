6b45f66f8c97fc7d2c4cfb812fa5d90ef90264e5
cmdlhz <cmdlhz@gmail.com>
2019-04-26 22:52:19 +0900

    [2-3] Week 2 models.py

--- backend/imagineer_django/week2/blog/models.py
+++ backend/imagineer_django/week2/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+  title = models.CharField(max_length=30)
+  contents = models.TextField()
+  view_count = models.IntegerField()
+
+class Comment(models.Model):
+  article = models.ForeignKey(Article, on_delete=models.CASCADE)
+  comment = models.CharField(max_length=100)
--- backend/imagineer_django/week2/week2/settings.py
+++ backend/imagineer_django/week2/week2/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add the name of the app
+    'blog'
 ]
 
 MIDDLEWARE = [
