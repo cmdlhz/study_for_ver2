b18543b54043d55639be67a6e986438d30eec312
cmdlhz <cmdlhz@gmail.com>
2019-04-27 17:09:11 +0900

    [2-3] Week 3 MTV Basic Setup
    Model-Template-View
    
    blog/models.py
    blog/urls.py
    blog/views.py
    templates/detail.html
    templates/navBar.html

--- backend/imagineer_django/week3/blog/models.py
+++ backend/imagineer_django/week3/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+  DEVELOPMENT = "DV"
+  PERSONAL = "PS"
+  CATEGORY_CHOICES = (
+    (DEVELOPMENT, "Development"),
+    (PERSONAL, "Personal")
+  )
+
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  category = models.CharField(
+    max_length=2,
+    chocies = CATEGORY_CHOICES,
+    default= DEVELOPMENT,
+  )
+
+class Comment(models.Model):
+  article = models.ForeignKey(Article, on_delete=models.CASCADE)
+  username = models.CharField(max_length=50)
+  content = models.CharField(max_length=200)
+
+class HashTag(models.Model):
+  name = models.CharField(max_length=50)
---
+++ backend/imagineer_django/week3/blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  # path('detail/', views.detail, name='detail'),
+  # path('about/', views.about, name='about'),
+]
