fcdb8f28b0db1dc76505b202c37acb2a4cc6533d
cmdlhz <cmdlhz@gmail.com>
2019-04-27 21:22:52 +0900

    [2-3] Week 3 Bootstrap (Part 3 of 3)

--- backend/imagineer_django/week3/blog/urls.py
+++ backend/imagineer_django/week3/blog/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
   path('', views.index, name='index'),
-  # path('detail/', views.detail, name='detail'),
+  re_path(r'^(?P<article_id>[0-9]+)/$', views.detail, name='detail'),
   # path('about/', views.about, name='about'),
 ]
