be68567ee4d5fe126840cb02d8ccafbaf756d7fc
cmdlhz <cmdlhz@gmail.com>
2019-04-29 23:29:41 +0900

    [2-3] Week 4 Signup (2 of 2)
    week4_baemin/urls.py
    templates/signup.html
    
    partner/views.py
    partner/urls.py
    
    static/css/base.css

--- backend/imagineer_django/week4_baemin/partner/urls.py
+++ backend/imagineer_django/week4_baemin/partner/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
-from .views import signup
+from django.urls import path, re_path, include
+from .views import index, signup
 
 app_name = 'partner'
 
 urlpatterns = [
-  path('', signup, name="signup")
+  path('', index, name="index"),
+  re_path(r'^signup/$', signup, name="signup")
 ]
--- backend/imagineer_django/week4_baemin/partner/views.py
+++ backend/imagineer_django/week4_baemin/partner/views.py
@@ -1,6 +1,19 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  ctx = {}
+  return render(request, 'index.html', ctx)
+
 def signup(request):
+  if request.method == 'GET':
+    pass
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user = User.objects.create_user(username, email, password)
+    user.save()
   ctx = {}
   return render(request, 'signup.html', ctx)
